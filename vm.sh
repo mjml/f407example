1c8a5bc1ef368d5f3af78e49ddedfd885ef980a8
Michael Joya <mike@michaeljoya.com>
2021-01-12 13:06:13 -0800

    now detaches usb device after guest shutdown

--- vm.sh
+++ vm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Which USB device do we pass through?
-address=$(lsusb | grep "Hermit's" | awk -e '{print $2 " " $4 }' | sed -e 's/\:$//' | sed -e 's/\b0*//g')
+address=$(lsusb | awk -e '/Hermit\x27s/ { print gensub(/\y0*/, "", "g", $2 " " gensub(/:$/, "", "g", $4)) }' | sort -k 2 | tail -1)
 busnum=$(echo $address | cut -f 1 -d \  -- )
 devnum=$(echo $address | cut -f 2 -d \  -- )
 
-echo address = $address
-echo busnum = $busnum
-echo devnum = $devnum
+echo USB Device scan found:
+echo address=$address
+echo busnum=$busnum, devnum=$devnum
 
 # Tell virsh about the USB device
 cat <<EOF > virt-usb.xml
@@ -22,3 +22,6 @@ virsh attach-device kdev virt-usb.xml --config
 
 # Start the actual VM
 virsh start kdev --console
+
+# After it's done, detach the device
+virsh detach-device kdev virt-usb.xml --config
